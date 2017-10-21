9275abb820fb32b7c159e516bc152c50d3a11b4c
chrisjxn <usjacksons@gmail.com>
2017-10-21 09:45:43 -0600

    commit changes

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = 3000,
+    bc = require('./controllers/books_controller');
+
+app.use(bodyParser.json());
+app.use(express.static( __dirname + '/../public/build'));
+
+app.post('/api/books', bc.create);
+app.get('/api/books', bc.read);
+app.put('/api/books/:id', bc.update);
+app.delete('/api/books/:id', bc.delete);
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
